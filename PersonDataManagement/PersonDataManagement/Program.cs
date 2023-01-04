3315ff9a818ecca3003208f53d619faa3ea48a3c
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-01-04 23:29:36 +0530

    [Sangmeshwar] Add: Person Data List Is Created

---
+++ PersonDataManagement/PersonDataManagement/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PersonDataManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Person Data Management!");
+            List<Person> Personlist = new List<Person>();
+            DataManagement.AddPersonDetails(Personlist);
+            DataManagement.IteratePersoninList(Personlist);
+        }
+    }
+}
